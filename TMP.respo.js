40bc35069cd05f75d55fd8f1427a6849a79016f9
iki1uc <entertrainer@web.de>
2026-08-07 11:04:15 +0200

    Update and rename TMP.respo to TMP.respo.js

---
+++ TMP.respo.js
@@ -0,0 +1,13 @@
+// TMP.respo.js
+export const TMP = {
+    center: "TMP",
+    freq: "12e",
+    axis: ["ROOT", "ACHSE", "TMP", "LAGE", "laTER", "ORT"],
+    last: {
+        score: 0,
+        wetten: 0,
+        qi: 0,
+        axis: ["ROOT"],
+        neutral: 0
+    }
+};
